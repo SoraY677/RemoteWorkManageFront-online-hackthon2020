06d240242a00e8f7b19d1a7e8ece7d3973a359d3
SoraY677 <SoraY677@outlook.com>
2020-03-29 19:58:55 +0900

    kanbanコンポーネント化

--- pages/index.vue
+++ pages/index.vue
@@ -1,45 +1,17 @@
 <template>
-  <div id="myKanban"></div>
+  <kanban />
 </template>
 
 <script>
+import kanban from "~/components/kanban"
 export default {
-  data() {
-    return {
-      defaultBoards: [
-        {
-          id: "sample-board-1",
-          title: "タスク",
-          class: "task",
-          
-          item: [{ title: "報告書の作成" }, { title: "14時から打ち合わせ" }]
-        },
-
-        {
-          id: "sample-board-2",
-          title: "進行中",
-          class: "progress",
-          item: [{ title: "○○案の企画書作成" }]
-        },
-
-        {
-          id: "sample-board-3",
-          title: "完了",
-          class: "done",
-          item: [{ title: "日報の提出" }]
-        }
-      ]
-    };
-  },
-  mounted() {
-    const kanban = new jKanban({
-      element: "#myKanban", //タスク管理ボードを表示するHTML要素
-      gutter: "15px", //ボード同士の間隔
-      widthBoard: "250px", //ボードのサイズ
-      boards: this.defaultBoards //初期状態のJSONデータ
-    });
+  components:{
+    kanban
   }
-};
+
+}
 </script>
 
-<style></style>
+<style>
+
+</style>
