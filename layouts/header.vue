f747c12fd268a66871592bef2ae4984d96729f8d
SoraY677 <SoraY677@outlook.com>
2020-03-29 20:26:20 +0900

    headerを適当に実装(全く動かない)

---
+++ layouts/header.vue
@@ -0,0 +1,34 @@
+<template>
+  <header class="nav-header">
+    <ul>
+      <li>ホーム</li>
+      <li>タスク管理</li>
+      <li>進捗グラフ</li>
+    </ul>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  header.nav-header{
+    background-color: #f44336;
+  }
+
+  header.nav-header > ul{
+    display: flex;
+    margin:0;
+    padding:20px;
+    /* justify-content: space-between; */
+    list-style: none;
+  }
+
+  header.nav-header > ul > li{
+    padding:0 .5em;
+    color:white;
+  }
+</style>
