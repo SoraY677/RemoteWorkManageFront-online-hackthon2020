c48b06a24332df0db2675f897a62d043998d1206
SoraY677 <SoraY677@outlook.com>
2020-03-30 13:24:07 +0900

    circleコンポーネント + α

--- components/achieveCircle.vue
+++ components/achieveCircle.vue
@@ -1,13 +1,50 @@
 <template>
-  <section>
+<div class="achive-circle-wrapper">
+  <svg class="c-circle__view" viewBox="0 0 63.6619772368 63.6619772368">
+    <circle
+      class="c-circle__1"
+      cx="31.8309886184"
+      cy="31.8309886184"
+      r="31.8309886184"
+    />
+  </svg>
+</div>
 </template>
 
 <script>
 export default {
-
-}
+  props: {
+    percentage: 0
+  }
+};
 </script>
 
 <style>
 
+.achive-circle-wrapper{
+  position: relative;
+  display: block;
+  height: 0;
+  font-size:1vw;
+  width: 100%;
+  padding-bottom: 100%;
+}
+.c-circle__view {
+  width: 100%;
+  height: 100%;
+  display: block;
+  position: absolute;
+  top: 0;
+  left: 0;
+  overflow: visible;
+}
+
+/* circle {
+    fill:rgba(0,0,0,0);
+  stroke-width:31.8309886184;
+  stroke-dasharray: 0,0,0,100;
+  stroke-dashoffset: 25;
+  -webkit-animation: pie1 3s infinite ease both;
+  animation: pie1 6s infinite ease both;
+} */
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -4,13 +4,18 @@
       <h1>全体の進捗ランキング</h1>
       <rankingBarchart :memberAchive="rankingJson" />
     </section>
-    <section class="">
+    <section class="achive-circle-area">
+      <h1>個人の達成度</h1>
+      <ul class="achieve-circles">
+      <li><achieveCircle /></li>
+      </ul>
     </section>
   </div>
 </template>
 
 <script>
 import rankingBarchart from "~/components/rankingBarchart";
+import achieveCircle from "~/components/achieveCircle"
 export default {
   data() {
     return {
@@ -27,7 +32,8 @@ export default {
     };
   },
   components: {
-    rankingBarchart
+    rankingBarchart,
+    achieveCircle
   }
 };
 </script>
@@ -36,4 +42,14 @@ export default {
 .ranking-barchart-area {
   padding: 0 10%;
 }
+
+ul.achieve-circles{
+  display: flex;
+  justify-content: space-between;
+}
+
+ul.achieve-circles > li{
+  width:30%;
+}
+
 </style>
