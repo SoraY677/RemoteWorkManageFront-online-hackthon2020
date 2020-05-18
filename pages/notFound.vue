15170d36bf1b42e597359498246e9824e168ecaf
SoraY677 <SoraY677@outlook.com>
2020-05-18 23:36:04 +0900

    404完了

--- pages/notFound.vue
+++ pages/notFound.vue
@@ -1,7 +1,16 @@
 <template>
-  <main>
-    404 not Found www
-  </main>
+  <v-sheet class="ma-10 pa-10 text-center" :tile="true">
+    <h1>404 not Found</h1>
+    <v-divider></v-divider>
+    <p class="mt-10">以下の原因が考えられます。</p>
+    <ul class="d-flex-wrap">
+      <li>・無効なページ遷移</li>
+      <li>・不正なURLへのアクセス</li>
+      <li>・認証失敗</li>
+      <v-btn to="/">ログインページはこちら</v-btn>
+    </ul>
+    <p></p>
+  </v-sheet>
 </template>
 
 <script>
