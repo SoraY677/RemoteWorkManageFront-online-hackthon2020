3ace049bf8a43efe6c72c595323c8811fff6b032
SoraY677 <SoraY677@outlook.com>
2020-05-24 14:57:58 +0900

    buttonを浮かす

--- pages/Manager/Task.vue
+++ pages/Manager/Task.vue
@@ -1,15 +1,23 @@
 <template>
   <v-card class="ma-10" outlined>
     <v-card-text class="text-center"> 現在のタスク一覧</v-card-text>
+    <v-btn
+      @click="showTaskDialog"
+      class="pa-2 float-top"
+      color="pink"
+      dark
+      right
+      fixed
+    >
+      <font-awesome-icon class="mr-1" :icon="['fas', 'plus']" />
+      taskを追加する
+    </v-btn>
     <v-divider></v-divider>
     <taskList
       class="ma-10"
       :content="JSON.parse(JSON.stringify(taskContents))"
     />
-    <v-btn @click="showTaskDialog" class="pa-2" color="pink" dark right fixed>
-      <font-awesome-icon class="mr-1" :icon="['fas', 'plus']" />
-      taskを追加する
-    </v-btn>
+
     <taskDialog ref="taskDialog" />
   </v-card>
 </template>
@@ -39,4 +47,7 @@ export default {
 </script>
 
 <style>
+.float-top {
+  z-index: 10;
+}
 </style>
