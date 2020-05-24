019240f1127d3c0e62162985078b9ad5191731fc
SoraY677 <SoraY677@outlook.com>
2020-05-24 14:47:49 +0900

    admin task をdoneをデザイン変更

--- components/allocateTask.vue
+++ components/allocateTask.vue
@@ -1,12 +1,21 @@
 <template>
   <v-card class="pa-2" :disabled="taskStatus.status">
     <v-card-title>
+      <v-chip
+        v-if="taskStatus.status"
+        class="float-right mr-2"
+        color="white"
+        :label="false"
+        text-color="green"
+        :outline="false"
+        >✔Done</v-chip
+      >
       <span class="mr-2">{{ taskStatus.do }}</span>
-      <v-chip color="green" text-color="white">
+      <v-chip color="red" text-color="white">
         <font-awesome-icon class="mr-1" :icon="['fas', 'user']" />
         {{ taskStatus.worker }}
-      </v-chip></v-card-title
-    >
+      </v-chip>
+    </v-card-title>
   </v-card>
 </template>
 
